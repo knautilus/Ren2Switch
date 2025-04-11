82cc2814e1445f99b524aa89f2a27181fdec8f98
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-04-11 22:48:34 +0300

    upd

--- setup.bash
+++ setup.bash
@@ -5,7 +5,7 @@ export RENPY_VER=7.6.3
 export PYGAME_SDL2_VER=2.1.0
 
 apt-get -y update
-apt-get -y install python3 python3-pip p7zip-full libsdl2-dev libsdl2-2.0-0 libsdl2-image-dev libsdl2-image-2.0-0 libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-ttf-2.0-0 libsdl2-mixer-dev libsdl2-mixer-2.0-0 libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
+apt-get -y install python3 python3-pip p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
 apt-get -y install python3-setuptools
 pip install future six typing requests ecdsa pefile==2019.4.18
 pip install Cython==0.29.37
