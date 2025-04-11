4d275d5bd9954b8df676a69eab76b04e9a816870
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-04-11 21:38:27 +0300

    SDL

--- setup.bash
+++ setup.bash
@@ -5,7 +5,7 @@ export RENPY_VER=7.6.3
 export PYGAME_SDL2_VER=2.1.0
 
 apt-get -y update
-apt-get -y install python3 python3-pip p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
+apt-get -y install python3 python3-pip git p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
 apt-get -y install python3-setuptools
 pip install future six typing requests ecdsa pefile==2019.4.18
 pip install Cython==0.29.37
@@ -63,3 +63,11 @@ popd
 pushd pygame_sdl2-source
 rm -rf gen gen-static
 popd
+
+git clone https://github.com/libsdl-org/SDL.git -b SDL2
+cd SDL
+mkdir build
+cd build
+../configure
+make
+sudo make install
