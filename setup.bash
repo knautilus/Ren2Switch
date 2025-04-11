ce71f12dca7afd1b1470942874b49c01bafdc831
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-04-11 18:21:12 +0300

    updated renpy version 7.6.3

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export RENPY_VER=7.6.1
+export RENPY_VER=7.6.3
 export PYGAME_SDL2_VER=2.1.0
 
 
@@ -14,7 +14,7 @@ curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/python27-sw
 curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
 dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-tar -xf python27-switch.tar.gz -C $DEVKITPRO/portlibs/switch
+tar -xvzf python27-switch.tar.gz -C $DEVKITPRO/portlibs/switch
 
 rm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
@@ -23,17 +23,17 @@ rm python27-switch.tar.gz
 /bin/bash -c 'sed -i'"'"'.bak'"'"' '"'"'s/set(CMAKE_EXE_LINKER_FLAGS_INIT "/set(CMAKE_EXE_LINKER_FLAGS_INIT "-fPIC /'"'"' $DEVKITPRO/switch.cmake'
 
 
-curl -LOC - https://www.renpy.org/dl/$RENPY_VER/pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER.tar.gz
+curl -LOC - https://www.renpy.org/dl/$RENPY_VER/pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-sdk.zip
 curl -LOC - https://www.renpy.org/dl/$RENPY_VER/renpy-$RENPY_VER-source.tar.bz2
-curl -LOC - https://www.renpy.org/dl/$RENPY_VER/android-native-symbols.zip
-curl -LOC - https://dl.otorh.in/github/rawproject.zip
+#curl -LOC - https://www.renpy.org/dl/$RENPY_VER/android-native-symbols.zip
+#curl -LOC - https://dl.otorh.in/github/rawproject.zip
 
 
-rm -rf pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER pygame_sdl2-source
-tar -xf pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER.tar.gz
-mv pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER pygame_sdl2-source
-rm pygame_sdl2-$PYGAME_SDL2_VER-for-renpy-$RENPY_VER.tar.gz
+rm -rf pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER pygame_sdl2-source
+tar -xvzf pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
+mv pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER pygame_sdl2-source
+rm pygame_sdl2-$PYGAME_SDL2_VER+renpy$RENPY_VER.tar.gz
 
 rm -rf renpy-$RENPY_VER-source renpy-source
 tar -xf renpy-$RENPY_VER-source.tar.bz2
@@ -44,14 +44,14 @@ rm -rf renpy-$RENPY_VER-sdk renpy_sdk
 unzip -qq renpy-$RENPY_VER-sdk.zip -d renpy_sdk
 rm renpy-$RENPY_VER-sdk.zip
 
-rm -rf raw
-unzip -qq rawproject.zip -d raw
-rm rawproject.zip
+#rm -rf raw
+#unzip -qq rawproject.zip -d raw
+#rm rawproject.zip
 
-rm -rf android-native-symbols renpy_androidlib ./raw/android/lib
-unzip -qq android-native-symbols.zip -d ./raw/android/lib
-rm -rf ./raw/android/lib/x86_64/
-rm android-native-symbols.zip
+#rm -rf android-native-symbols renpy_androidlib ./raw/android/lib
+#unzip -qq android-native-symbols.zip -d ./raw/android/lib
+#rm -rf ./raw/android/lib/x86_64/
+#rm android-native-symbols.zip
 
 pushd renpy-source
 patch -p1 < ../renpy.patch
