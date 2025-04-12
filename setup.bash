456d7bcb6313b3a45c3749d6c7b44500b3109cc3
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-04-12 06:22:04 +0300

    ?

--- setup.bash
+++ setup.bash
@@ -10,16 +10,16 @@ apt-get -y install python3-setuptools
 pip install future six typing requests ecdsa pefile==2019.4.18
 pip install Cython==0.29.37
 
-curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
-curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/python27-switch.tar.gz
-curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
-dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-tar -xvzf python27-switch.tar.gz -C $DEVKITPRO/portlibs/switch
+#curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
+#curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/python27-switch.tar.gz
+#curl -LOC - https://github.com/Otorhin/scripts/releases/download/oof/switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
+#dkp-pacman -U --noconfirm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#tar -xvzf python27-switch.tar.gz -C $DEVKITPRO/portlibs/switch
 
-rm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
-rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
-rm python27-switch.tar.gz
+#rm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
+#rm switch-libfribidi-1.0.12-1-any.pkg.tar.xz
+#rm python27-switch.tar.gz
 
 /bin/bash -c 'sed -i'"'"'.bak'"'"' '"'"'s/set(CMAKE_EXE_LINKER_FLAGS_INIT "/set(CMAKE_EXE_LINKER_FLAGS_INIT "-fPIC /'"'"' $DEVKITPRO/switch.cmake'
 
@@ -45,7 +45,7 @@ rm -rf renpy-$RENPY_VER-sdk renpy_sdk
 unzip -qq renpy-$RENPY_VER-sdk.zip -d renpy_sdk
 rm renpy-$RENPY_VER-sdk.zip
 
-dkp-pacman -S --noconfirm switch-sdl2
+#dkp-pacman -S --noconfirm switch-sdl2
 
 #rm -rf raw
 #unzip -qq rawproject.zip -d raw
