d39a1bb481ae77097f11ad177128658e59fd2a3d
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-05-21 23:18:31 +0300

    ?

--- build.bash
+++ build.bash
@@ -92,7 +92,7 @@ rm -rf private
 
 
 mkdir -p ./raw/switch/romfs/Contents/renpy
-mkdir -p ./raw/lib
+mkdir -p ./raw/lib/sw
 #mkdir -p ./raw/android/assets/renpy/common
 cp -r ./renpy_clear/renpy/common ./raw/switch/romfs/Contents/renpy/
 #cp -r ./renpy_clear/renpy/common ./raw/android/assets/renpy/
@@ -100,7 +100,7 @@ cp -r ./renpy_clear/renpy/common ./raw/switch/romfs/Contents/renpy/
 cp ./renpy_clear/renpy.py ./raw/switch/romfs/Contents/
 #unzip -qq ./raw/lib.zip -d ./raw/lib/
 #rm ./raw/lib.zip
-cp $LOCAL_PREFIX/lib/librenpy-switch-modules.a ./raw/lib
+cp $DEVKITPRO/portlibs/switch/. ./raw/lib/sw
 cp -r ./renpy_clear/lib/python3.9/. ./raw/lib
 cp -r ./renpy_clear/renpy ./raw/lib
 rm -rf ./raw/lib/renpy/common/
