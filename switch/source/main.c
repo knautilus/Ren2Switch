09ebff759272e3c9d26827ca48e366da309f5bea
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-05-23 00:39:22 +0300

    ?

--- switch/source/main.c
+++ switch/source/main.c
@@ -465,7 +465,7 @@ int main(int argc, char* argv[])
     PyConfig_Clear(&config);
 
     show_error("before PyRun_SimpleString", 0);
-    python_result = PyRun_SimpleString("import sys; sys.path = ['romfs:/Contents/lib'];");
+    python_result = PyRun_SimpleString("import sys; sys.path.insert('romfs:/Contents/lib');");
     if (python_result == -1)
     {
         show_error("Could not set the Python path.\n\nThis is an internal error and should not occur during normal usage.", 1);
